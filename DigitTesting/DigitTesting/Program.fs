a1c0642974b7df11035463ae5b2d935ad1835b32
Olgierd Dziamski <olgierd2d@gmail.com>
2015-09-12 14:42:38 -0400

    Fixed bug at add function

--- DigitTesting/DigitTesting/Program.fs
+++ DigitTesting/DigitTesting/Program.fs
@@ -1,11 +1,11 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
-add x y = x + y
+let add x y = x + y
 
 [<EntryPoint>]
 let main argv = 
     printfn "Hallow Word"
     let sum = add 3 4
     printfn "%i" sum
-    0 // return an integer exit code
+    0 // return an integer exit code 
